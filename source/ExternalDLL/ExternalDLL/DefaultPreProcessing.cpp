cde51030feab6a35ad951218c37127fa3ff9cfd0
FaizalSupriadi <faizal.faizalrachmansupriadi@student.hu.nl>
2020-03-06 11:18:39 +0100

    documentation

--- source/ExternalDLL/ExternalDLL/DefaultPreProcessing.cpp
+++ source/ExternalDLL/ExternalDLL/DefaultPreProcessing.cpp
@@ -33,14 +33,21 @@ IntensityImage * DefaultPreProcessing::stepScaleImage(const IntensityImage &src)
 }
 
 IntensityImage * DefaultPreProcessing::stepEdgeDetection(const IntensityImage &src) const {
+	// Image container
 	cv::Mat OverHillOverDale;
+	// Maak een column van src en stop dat in overhill
 	HereBeDragons::HerLoveForWhoseDearLoveIRiseAndFall(src, OverHillOverDale);
 	//cv::medianBlur(*image, *image, 3);
 	//cv::GaussianBlur(*image, *image, cv::Size(3, 3), 0, 0, cv::BORDER_DEFAULT);
+	// LaplaceKernel
 	cv::Mat ThoroughBushThoroughBrier = (cv::Mat_<float>(9, 9) << 0, 0, 0, 1, 1, 1, 0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0, 1, 1, 1, -4, -4, -4, 1, 1, 1, 1, 1, 1, -4, -4, -4, 1, 1, 1, 1, 1, 1, -4, -4, -4, 1, 1, 1, 0, 0, 0, 1, 1, 1, 0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0);
+	// Image container als output
 	cv::Mat OverParkOverPale;
+	// Convuleer de afbeelding met de kernel, overhill is input, overpark is output
 	filter2D(OverHillOverDale, OverParkOverPale, CV_8U, ThoroughBushThoroughBrier, cv::Point(-1, -1), 0, cv::BORDER_DEFAULT);
+	// creates a new empty intensity image
 	IntensityImage * ThoroughFloodThoroughFire = ImageFactory::newIntensityImage();
+	// Maak er weer een rij van en stop het in thorough
 	HereBeDragons::NoWantOfConscienceHoldItThatICall(OverParkOverPale, *ThoroughFloodThoroughFire);
 	return ThoroughFloodThoroughFire;
 }
