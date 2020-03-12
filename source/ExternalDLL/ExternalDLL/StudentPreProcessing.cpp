6adbfd59eb9d5cc23a112dc22f844679ddc58818
Menno <mvdjagt1@gmail.com>
2020-03-12 17:22:18 +0100

    resolved an error and added new code
    tried to implement prewitt method

--- source/ExternalDLL/ExternalDLL/StudentPreProcessing.cpp
+++ source/ExternalDLL/ExternalDLL/StudentPreProcessing.cpp
@@ -15,20 +15,31 @@ IntensityImage * StudentPreProcessing::stepScaleImage(const IntensityImage &imag
 IntensityImage * StudentPreProcessing::stepEdgeDetection(const IntensityImage &src) const {
 	// Image container
 	cv::Mat OverHillOverDale;
-	// Maak een column van src en stop dat in overhill
-	HereBeDragons::HerLoveForWhoseDearLoveIRiseAndFall(src, OverHillOverDale);
-	//cv::medianBlur(*image, *image, 3);
-	//cv::GaussianBlur(*image, *image, cv::Size(3, 3), 0, 0, cv::BORDER_DEFAULT);
-	// LaplaceKernel
-	cv::Mat ThoroughBushThoroughBrier = (cv::Mat_<float>(3, 3) <<  1,0,-1, 1, 0, -1, 1, 0, -1 );
+	//HereBeDragons::HerLoveForWhoseDearLoveIRiseAndFall(src, OverHillOverDale);
+
+	int w = src.getWidth();
+	int h = src.getHeight();
+
+	OverHillOverDale.create(h, w, CV_8UC1);
+
+	for (int x = 0; x < OverHillOverDale.cols; x++) {
+		for (int y = 0; y < OverHillOverDale.rows; y++) {
+			OverHillOverDale.at<uchar>(y, x) = src.getPixel(x, y);
+		}
+	}
+
+	cv::Mat prewittx = (cv::Mat_<float>(3, 3) << 1, 0, -1, 1, 0, -1, 1, 0, -1 );
+	cv::Mat prewitty = (cv::Mat_<float>(3, 3) << 1, 1, 1, 0, 0, 0, -1, -1, -1 );
 	// Image container als output
-	cv::Mat OverParkOverPale;
+	cv::Mat prewittxResult;
+	cv::Mat prewittyResult;
 	// Convuleer de afbeelding met de kernel, overhill is input, overpark is output
-	filter2D(OverHillOverDale, OverParkOverPale, CV_8U, ThoroughBushThoroughBrier, cv::Point(-1, -1), 0, cv::BORDER_DEFAULT);
+	filter2D(OverHillOverDale, prewittxResult, CV_8U, prewittx, cv::Point(-1, -1));
+	filter2D(OverHillOverDale, prewittyResult, CV_8U, prewitty, cv::Point(-1, -1));
 	// creates a new empty intensity image
 	IntensityImage* ThoroughFloodThoroughFire = ImageFactory::newIntensityImage();
 	// Maak er weer een rij van en stop het in thorough
-	HereBeDragons::NoWantOfConscienceHoldItThatICall(OverParkOverPale, *ThoroughFloodThoroughFire);
+	HereBeDragons::NoWantOfConscienceHoldItThatICall(prewittxResult + prewittyResult, *ThoroughFloodThoroughFire);
 	return ThoroughFloodThoroughFire;
 }
 
