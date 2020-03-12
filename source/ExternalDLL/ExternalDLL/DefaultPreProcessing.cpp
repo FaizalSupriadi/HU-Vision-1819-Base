b8568d815efd97ad44fd550e8365d732afb5bc28
Menno <mvdjagt1@gmail.com>
2020-03-12 12:32:16 +0100

    merged faizals branch

--- source/ExternalDLL/ExternalDLL/DefaultPreProcessing.cpp
+++ source/ExternalDLL/ExternalDLL/DefaultPreProcessing.cpp
@@ -33,23 +33,35 @@ IntensityImage * DefaultPreProcessing::stepScaleImage(const IntensityImage &src)
 }
 
 IntensityImage * DefaultPreProcessing::stepEdgeDetection(const IntensityImage &src) const {
+	// Image container
 	cv::Mat OverHillOverDale;
+	// Maak een column van src en stop dat in overhill
 	HereBeDragons::HerLoveForWhoseDearLoveIRiseAndFall(src, OverHillOverDale);
 	//cv::medianBlur(*image, *image, 3);
 	//cv::GaussianBlur(*image, *image, cv::Size(3, 3), 0, 0, cv::BORDER_DEFAULT);
+	// LaplaceKernel
 	cv::Mat ThoroughBushThoroughBrier = (cv::Mat_<float>(9, 9) << 0, 0, 0, 1, 1, 1, 0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0, 1, 1, 1, -4, -4, -4, 1, 1, 1, 1, 1, 1, -4, -4, -4, 1, 1, 1, 1, 1, 1, -4, -4, -4, 1, 1, 1, 0, 0, 0, 1, 1, 1, 0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0);
+	// Image container als output
 	cv::Mat OverParkOverPale;
+	// Convuleer de afbeelding met de kernel, overhill is input, overpark is output
 	filter2D(OverHillOverDale, OverParkOverPale, CV_8U, ThoroughBushThoroughBrier, cv::Point(-1, -1), 0, cv::BORDER_DEFAULT);
-	IntensityImage * ThoroughFloodThoroughFire = ImageFactory::newIntensityImage();
+	// creates a new empty intensity image
+	IntensityImage* ThoroughFloodThoroughFire = ImageFactory::newIntensityImage();
+	// Maak er weer een rij van en stop het in thorough
 	HereBeDragons::NoWantOfConscienceHoldItThatICall(OverParkOverPale, *ThoroughFloodThoroughFire);
 	return ThoroughFloodThoroughFire;
 }
 
 IntensityImage * DefaultPreProcessing::stepThresholding(const IntensityImage &src) const {
+	// Image container
 	cv::Mat OverHillOverDale;
+	// Maak een column van src en stop dat in overhill
 	HereBeDragons::HerLoveForWhoseDearLoveIRiseAndFall(src, OverHillOverDale);
+	// Treshold is 220 and the max value is 255, overhill is input en output.
 	cv::threshold(OverHillOverDale, OverHillOverDale, 220, 255, cv::THRESH_BINARY_INV);
-	IntensityImage * ThoroughBushThoroughBrier = ImageFactory::newIntensityImage();
-	HereBeDragons::NoWantOfConscienceHoldItThatICall(OverHillOverDale, *ThoroughBushThoroughBrier);
+	// creates a new empty intensity image
+	IntensityImage* ThoroughFloodThoroughFire = ImageFactory::newIntensityImage();
+	// Maak er weer een rij van en stop het in thorough
+	HereBeDragons::NoWantOfConscienceHoldItThatICall(OverParkOverPale, *ThoroughFloodThoroughFire);
 	return ThoroughBushThoroughBrier;
 }
