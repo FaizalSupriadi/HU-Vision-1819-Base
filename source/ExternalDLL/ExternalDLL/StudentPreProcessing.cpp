1bdc14399066c4d64198ae9a4abd3448d16f69f3
Mennovdjagt <43231296+Mennovdjagt@users.noreply.github.com>
2020-03-16 14:13:59 +0100

    added better names

--- source/ExternalDLL/ExternalDLL/StudentPreProcessing.cpp
+++ source/ExternalDLL/ExternalDLL/StudentPreProcessing.cpp
@@ -14,19 +14,8 @@ IntensityImage * StudentPreProcessing::stepScaleImage(const IntensityImage &imag
 
 IntensityImage * StudentPreProcessing::stepEdgeDetection(const IntensityImage &src) const {
 	// Image container
-	cv::Mat OverHillOverDale;
-	//HereBeDragons::HerLoveForWhoseDearLoveIRiseAndFall(src, OverHillOverDale);
-
-	int w = src.getWidth();
-	int h = src.getHeight();
-
-	OverHillOverDale.create(h, w, CV_8UC1);
-
-	for (int x = 0; x < OverHillOverDale.cols; x++) {
-		for (int y = 0; y < OverHillOverDale.rows; y++) {
-			OverHillOverDale.at<uchar>(y, x) = src.getPixel(x, y);
-		}
-	}
+	cv::Mat image;
+	HereBeDragons::HerLoveForWhoseDearLoveIRiseAndFall(src, image);
 
 	cv::Mat prewittx = (cv::Mat_<float>(3, 3) << 1, 0, -1, 1, 0, -1, 1, 0, -1 );
 	cv::Mat prewitty = (cv::Mat_<float>(3, 3) << 1, 1, 1, 0, 0, 0, -1, -1, -1 );
@@ -34,13 +23,13 @@ IntensityImage * StudentPreProcessing::stepEdgeDetection(const IntensityImage &s
 	cv::Mat prewittxResult;
 	cv::Mat prewittyResult;
 	// Convuleer de afbeelding met de kernel, overhill is input, overpark is output
-	filter2D(OverHillOverDale, prewittxResult, CV_8U, prewittx, cv::Point(-1, -1));
-	filter2D(OverHillOverDale, prewittyResult, CV_8U, prewitty, cv::Point(-1, -1));
+	filter2D(image, prewittxResult, CV_8U, prewittx, cv::Point(-1, -1));
+	filter2D(image, prewittyResult, CV_8U, prewitty, cv::Point(-1, -1));
 	// creates a new empty intensity image
-	IntensityImage* ThoroughFloodThoroughFire = ImageFactory::newIntensityImage();
+	IntensityImage* endResult = ImageFactory::newIntensityImage();
 	// Maak er weer een rij van en stop het in thorough
-	HereBeDragons::NoWantOfConscienceHoldItThatICall(prewittxResult + prewittyResult, *ThoroughFloodThoroughFire);
-	return ThoroughFloodThoroughFire;
+	HereBeDragons::NoWantOfConscienceHoldItThatICall(prewittxResult + prewittyResult, *endResult);
+	return endResult;
 }
 
 IntensityImage * StudentPreProcessing::stepThresholding(const IntensityImage &image) const {
