f4d7daedeaffad8f5a20125b268ff5fb5628f380
FaizalSupriadi <43231255+FaizalSupriadi@users.noreply.github.com>
2020-03-21 16:35:55 +0100

    Changed to memory experiment code

--- source/ExternalDLL/ExternalDLL/StudentPreProcessing.cpp
+++ source/ExternalDLL/ExternalDLL/StudentPreProcessing.cpp
@@ -3,63 +3,94 @@
 #include "GrayscaleAlgorithm.h"
 #include "ImageFactory.h"
 #include "HereBeDragons.h"
+#include <iostream>
+#include <math.h>
+#include <array>
+#include <chrono>
 
-IntensityImage * StudentPreProcessing::stepToIntensityImage(const RGBImage &image) const {
+using namespace std::chrono;
+
+IntensityImage* StudentPreProcessing::stepToIntensityImage(const RGBImage& image) const {
 	return nullptr;
 }
 
-IntensityImage * StudentPreProcessing::stepScaleImage(const IntensityImage &image) const {
+IntensityImage* StudentPreProcessing::stepScaleImage(const IntensityImage& image) const {
 	return nullptr;
 }
 
-IntensityImage * StudentPreProcessing::stepEdgeDetection(const IntensityImage &src) const {
-	// Image container
-	cv::Mat imageContainer;
+IntensityImage* StudentPreProcessing::stepEdgeDetection(const IntensityImage& src) const {
+	
+	/*
+	//microseconds totalDuration = milliseconds(0);
 
-	HereBeDragons::HerLoveForWhoseDearLoveIRiseAndFall(src, imageContainer);
+	//for (int i = 0; i < 1000; i++) {
+	//	auto start = high_resolution_clock::now();
 
-	float weight = 3;
+		cv::Mat imageContainer;
+		HereBeDragons::HerLoveForWhoseDearLoveIRiseAndFall(src, imageContainer);
 
-	/*cv::Mat prewittx = (cv::Mat_<float>(3, 3) << 1*weight, 0, -1*weight, 1*weight, 0, -1*weight, 1*weight, 0, -1*weight);
-	cv::Mat prewitty = (cv::Mat_<float>(3, 3) << -1*weight, -1*weight, -1*weight, 0, 0, 0, 1*weight, 1*weight, 1*weight);
-	cv::Mat image;
-	HereBeDragons::HerLoveForWhoseDearLoveIRiseAndFall(src, image);
-	cv::Mat prewittx = (cv::Mat_<float>(3, 3) << 1, 0, -1, 1, 0, -1, 1, 0, -1 );
-	cv::Mat prewitty = (cv::Mat_<float>(3, 3) << 1, 1, 1, 0, 0, 0, -1, -1, -1 );
-	// Image container als output
-	cv::Mat prewittxResult;
-	cv::Mat prewittyResult;
-	// Convuleer de afbeelding met de kernel, overhill is input, overpark is output
-	filter2D(imageContainer, prewittxResult, -1, prewittx, cv::Point(-1, -1));
-	filter2D(imageContainer, prewittyResult, -1, prewitty, cv::Point(-1, -1));
-	// creates a new empty intensity image
-	IntensityImage* edgeDetectionImage = ImageFactory::newIntensityImage();
-	HereBeDragons::NoWantOfConscienceHoldItThatICall(prewittxResult + prewittyResult, *edgeDetectionImage);*/
+		cv::Mat prewittx = (cv::Mat_<float>(3, 3) << 1, 0, -1, 1, 0, -1, 1, 0, -1);
+		cv::Mat prewitty = (cv::Mat_<float>(3, 3) << 1, 1, 1, 0, 0, 0, -1, -1, -1);
+
+		cv::Mat prewittxResult;
+		cv::Mat prewittyResult;
 
 
-	//cv::Mat sobelx = (cv::Mat_<float>(3, 3) << -1, 0, 1, -2 * weight, 0, 2 * weight, -1, 0, 1);
-	//cv::Mat sobely = (cv::Mat_<float>(3, 3) << -1, -2 * weight, -1, 0, 0, 0, 1, 2 * weight, 1);
+		filter2D(imageContainer, prewittxResult, -1, prewittx, cv::Point(-1, -1));
+		filter2D(imageContainer, prewittyResult, -1, prewitty, cv::Point(-1, -1));
 
-	cv::Mat sobelx = (cv::Mat_<float>(3, 3) << -1, 0, 1, -2 , 0, 2, -1, 0, 1);
-	cv::Mat sobely = (cv::Mat_<float>(3, 3) << -1, -2 , -1, 0, 0, 0, 1, 2 , 1);
+		cv::Mat weighted;
+		cv::addWeighted(prewittxResult, 3, prewittyResult, 3, 0, weighted, -1);
 
-	cv::Mat sobelxResult;
-	cv::Mat sobelyResult;
+		IntensityImage* edgeDetectionImage = ImageFactory::newIntensityImage();
+
+		HereBeDragons::NoWantOfConscienceHoldItThatICall(weighted, *edgeDetectionImage);
+
+		return edgeDetectionImage;
+
+	//	auto stop = high_resolution_clock::now();
+	//	totalDuration += duration_cast<microseconds>(stop - start);
+	//}
+	std::cout << "-----------------------------" << totalDuration.count() / 1000 << std::endl;
+	*/
+	
+	
+
 
 	
-	filter2D(imageContainer, sobelxResult, -1, sobelx, cv::Point(-1, -1));
-	filter2D(imageContainer, sobelyResult, -1, sobely, cv::Point(-1, -1));
+	//microseconds totalDuration = milliseconds(0);
 
-	IntensityImage* edgeDetectionImage = ImageFactory::newIntensityImage();
-	cv::Mat weighted;
-	// Weighted both by 3, experiment with numbers
-	cv::addWeighted(sobelxResult, 3, sobelyResult, 3, 0, weighted,-1);
-	HereBeDragons::NoWantOfConscienceHoldItThatICall(weighted, *edgeDetectionImage);
+//	for (int i = 0; i < 1000; i++) {
+		//auto start = high_resolution_clock::now();
 
+		cv::Mat imageContainer;
+		HereBeDragons::HerLoveForWhoseDearLoveIRiseAndFall(src, imageContainer);
+
+		cv::Mat sobelx = (cv::Mat_<float>(3, 3) << -1, 0, 1, -2, 0, 2, -1, 0, 1);
+		cv::Mat sobely = (cv::Mat_<float>(3, 3) << -1, -2, -1, 0, 0, 0, 1, 2, 1);
+
+		cv::Mat sobelxResult;
+		cv::Mat sobelyResult;
+
+		filter2D(imageContainer, sobelxResult, -1, sobelx, cv::Point(-1, -1));
+		filter2D(imageContainer, sobelyResult, -1, sobely, cv::Point(-1, -1));
+
+		cv::Mat weighted;
+		cv::addWeighted(sobelxResult, 3, sobelyResult, 3, 0, weighted, -1);
+
+		IntensityImage* edgeDetectionImage = ImageFactory::newIntensityImage();
+
+		HereBeDragons::NoWantOfConscienceHoldItThatICall(weighted, *edgeDetectionImage);
+
+	//	auto stop = high_resolution_clock::now();
+	//	totalDuration += duration_cast<microseconds>(stop - start);
+	//}
+//	std::cout << "-----------------------------" << totalDuration.count()/1000 << std::endl;
+	
 	return edgeDetectionImage;
 }
 
-IntensityImage * StudentPreProcessing::stepThresholding(const IntensityImage &src) const {
+IntensityImage* StudentPreProcessing::stepThresholding(const IntensityImage& src) const {
 	// Image container
 	cv::Mat container;
 	// Maak een column van src en stop dat in overhill
@@ -70,13 +101,14 @@ IntensityImage * StudentPreProcessing::stepThresholding(const IntensityImage &sr
 	//cv::threshold(container, container, 220, 255, cv::THRESH_BINARY_INV);
 	//cv.adaptiveThreshold(container,container, 255, cv::ADAPTIVE_THRESH_MEAN_C, cv::THRESH_BINARY, 11, 2);
 	// Adaptive treshold, de minimum wordt door de gaussian treshold bepaalt.
-    //cv::adaptiveThreshold(container, container, 255, cv::ADAPTIVE_THRESH_GAUSSIAN_C, cv::THRESH_BINARY, 15, 10);
+	//cv::adaptiveThreshold(container, container, 255, cv::ADAPTIVE_THRESH_GAUSSIAN_C, cv::THRESH_BINARY, 15, 10);
 	cv::GaussianBlur(container, container, cv::Size(5, 5), 0);
 	cv::threshold(container, container, 0, 255, cv::THRESH_BINARY + cv::THRESH_OTSU);
 	// creates a new empty intensity image
 	IntensityImage* intensity = ImageFactory::newIntensityImage();
 	// Maak er weer een rij van en stop het in thorough
 	// Convert back
-	HereBeDragons::NoWantOfConscienceHoldItThatICall(container ,*intensity);
+	HereBeDragons::NoWantOfConscienceHoldItThatICall(container, *intensity);
 	return intensity;
 }
+
